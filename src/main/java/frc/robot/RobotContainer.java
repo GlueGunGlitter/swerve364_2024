581287e69efd3d2598d9d96836850508f06f4a58
G3 <g3@shvilim.org>
2024-02-20 22:47:23 +0200

    transportation and swerver rotation ( MIGHT NOT WORK )

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,12 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -15,7 +18,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.automations.*;
 
 import frc.robot.autos.*;
@@ -33,9 +36,12 @@ import frc.robot.subsystems.*;
  */
 public class RobotContainer {
 
+    public static double translationVal;
+    public static double strafeVal;
+
     /* Controllers */
     public static final XboxController xboxController = new XboxController(0);
-    private final Joystick driver = new Joystick(0);
+    private final static Joystick driver = new Joystick(0);
 
     /* Drive Controls */
     private final int translationAxis = XboxController.Axis.kLeftY.value;
@@ -43,9 +49,15 @@ public class RobotContainer {
     private final int rotationAxis = XboxController.Axis.kRightX.value;
 
     /* Driver Buttons */
+
     private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
-    private final JoystickButton testCmd = new JoystickButton(driver, XboxController.Button.kX.value);
+    private final JoystickButton shootUpButton = new JoystickButton(driver, XboxController.Button.kA.value);
+
+    POVButton d_Uppov = new POVButton(driver, 0);
+    POVButton d_Rightpov = new POVButton(driver, 90);
+    POVButton d_Downpov = new POVButton(driver, 180);
+    POVButton d_Leftpov = new POVButton(driver, 270);
 
     // private final JoystickButton IntakeEnableCommand = new JoystickButton(driver,
     // XboxController.Button.kRightBumper.value);
@@ -63,6 +75,7 @@ public class RobotContainer {
      * for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
+
         // Build an auto chooser. This will use Commands.none() as the default option.
         // Command intake = Commands.run(() -> m_TransportationSubsystem.setSpeed(0.6,
         // 0.6, 1));
@@ -100,9 +113,10 @@ public class RobotContainer {
     private void configureButtonBindings() {
         /* Driver Buttons */
         zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
-        testCmd.onTrue(new enableHighShooter());
-        // IntakeEnableCommand.onTrue(Commands.sequence(intake));
+
     }
+
+    // IntakeEnableCommand.onTrue(Commands.sequence(intake));
 
     private void registerCommands() {
         NamedCommands.registerCommand("HighShooter", new enableHighShooter());
--- src/main/java/frc/robot/subsystems/TransportationSubsystem.java
+++ src/main/java/frc/robot/subsystems/TransportationSubsystem.java
@@ -6,15 +6,22 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class TransportationSubsystem extends SubsystemBase {
-  WPI_TalonSRX intakeLowerMotor = new WPI_TalonSRX(Constants.TransportationConstants.INTAKE_LOWER_MOTOR_PORT);
-  WPI_VictorSPX intakeHigherMotor = new WPI_VictorSPX(Constants.TransportationConstants.INTAKE_HIGHER_MOTOR_PORT);
-  WPI_TalonSRX transportationMotor = new WPI_TalonSRX(Constants.TransportationConstants.TRANSPORTATION_MOTOR_PORT);
+  CANSparkFlex intakeHigherMotor = new CANSparkFlex(Constants.TransportationConstants.INTAKE_HIGHER_MOTOR_PORT,
+      MotorType.kBrushless);
+  CANSparkFlex intakeLowerMotor = new CANSparkFlex(Constants.TransportationConstants.INTAKE_LOWER_MOTOR_PORT,
+      MotorType.kBrushless);
+  CANSparkFlex transportationMotor = new CANSparkFlex(Constants.TransportationConstants.TRANSPORTATION_MOTOR_PORT,
+      MotorType.kBrushless);
+  CANSparkFlex transportationMotor2 = new CANSparkFlex(Constants.TransportationConstants.TRANSPORTATION_MOTOR_PORT,
+      MotorType.kBrushless);
 
   /** Creates a new IntakeSubsystem. */
   public TransportationSubsystem() {
